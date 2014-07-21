9fd57667da325156a74c258bc988a9422a3e167b
root <root@uofthacks.com>
2014-07-20 23:27:27 -0400

    Use absolute path to dokku command

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every 1.day, at: '4:00 am' do
-  command "dokku run staging rake db:backup"
-  command "dokku run uofthacks.com rake db:backup"
+  command "/usr/local/bin/dokku run staging rake db:backup"
+  command "/usr/local/bin/dokku run uofthacks.com rake db:backup"
 end
